fbd51bde1257b73fdde43589482e0252f820b2c0
Vitalii Kovalchuk <vtl.kovalchuk@gmail.com>
2021-12-07 14:54:37 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+vitalii; kovalchul; one nore;
+
+app.get('/', (req, res) => {
+    res.send()
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running...')
+}) 
